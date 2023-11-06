6ae8ff939615d97b6f42d9627654efe77914aeb8
David Inglés <davidingleslopez@gmail.com>
2023-11-06 17:13:44 +0100

    primer commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+import styles from './ui/home.module.css'
 
 import Link from 'next/link';
+import { lusitana } from './ui/fonts';
+import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className={`${lusitana.className} flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52`}>HOLA mi tipografía es lusitana
         {/* <AcmeLogo /> */}
+        <div className={styles.shape}></div>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong style={{ border: '1px solid black' }}>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
@@ -24,9 +28,22 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src='/hero-desktop.png'
+            alt='hero'
+            width={2000 / 2}
+            height={1520 / 2}
+            className='hidden md:block'
+          />
+          <Image
+            src='/hero-mobile.png'
+            alt='hero'
+            width={2000 / 4}
+            height={1520 / 4}
+            className='md:hidden block'
+          />
         </div>
       </div>
-    </main>
+    </main >
   );
 }
