d6be14063c748468b0d9155a222ac404ff85f656
David Inglés <davidingleslopez@gmail.com>
2023-11-07 11:45:23 +0100

    terminado y funcionando---suspense y skeleton y loading

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,12 @@
+import { Suspense } from "react";
 import { fetchLatestInvoices, fetchRevenue } from "../lib/data";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import { lusitana } from "../ui/fonts";
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from "../ui/skeletons";
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices()
+
 
   return (
     <main>
@@ -23,8 +24,12 @@ export default async function Page() {
         /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
